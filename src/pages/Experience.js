ddfcb680633dcbf250c60dbf9003f937be9139d1
Sourabh <sr925041@dal.ca>
2023-08-05 23:56:57 -0300

    about page

---
+++ src/pages/Experience.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Experience = () => {
+  return (
+    <div>
+      <h2>Work Experience</h2>
+      {/* Add your work experience details here */}
+      <h2>Projects</h2>
+      {/* Add your projects details here */}
+    </div>
+  );
+};
+
+export default Experience;
